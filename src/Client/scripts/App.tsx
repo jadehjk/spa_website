be93bae425e33bf8f0814021fe6151e6915d6d79
Jade Kim <jadekim@Jades-MacBook-Pro.local>
2020-07-28 20:21:18 -0400

    [feature] added app bar

--- src/Client/scripts/App.tsx
+++ src/Client/scripts/App.tsx
@@ -4,6 +4,11 @@ import Constants from './Constants';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
 import RouteWithNavbar from './Wrappers/RouteWithNavbar';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import Projects from './components/Projects';
 
 
@@ -22,6 +27,15 @@ class App extends React.Component<{}, {}> {
    render() {
       return (
          <BrowserRouter>
+            <AppBar position="static">
+               <Toolbar>
+              
+                  <Typography variant="h6"> Hi Jason </Typography>
+                  <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+               </Toolbar>
+            </AppBar>
             <Switch>
               <RouteWithNavbar exact path={home} component={Home} />
               <RouteWithNavbar exact path={aboutMe} component={AboutMe} />
