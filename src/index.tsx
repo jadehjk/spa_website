0cf97967621c8f1a027a5ed24a7c3a199baaa65d
Jason <eunpyo@umich.edu>
2020-08-04 16:23:24 +0900

    Fixed extra body width bug, changed video to non autoplay but with thumbnail

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import App from './Client/scripts/App';
 import './Client/Styles/index.scss'
 
@@ -7,8 +11,3 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
